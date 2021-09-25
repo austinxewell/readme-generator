981b28f0ffbf8273a71b6afd7ab56911ca05927d
AustinxEwell <austin.ewell86@gmail.com>
2021-09-24 22:56:50 -0600

    worked in study group to refactor code and fix major issues with code

--- dist/readme.md
+++ dist/readme.md
@@ -1,3 +1,28 @@
-
-  read me is working
+# Professional Read Me Generator 
+  [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+  # Description
+  This project uses node.js to cycle through a prompt to gather information to place inside of a readme.md file.
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  # Installation
+  none
+  # Usage
+  run node.js to gather data. Click on the README.MD file that is created in the dist folder.
+  # License
+  ## The Unlicense
+  A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.
+  View terms and conditions here: [The Unlicense](../utils/licenses/unlicense.txt)
+  # Contributing
+  nick jesnson
+  # Tests
+  run test by doing tests
+  # Questions
+  * GitHub user: austinxewell
+  * https://github.com/austinxewell
+  * Reach me at austin.ewell86@gmail.com for additional questions
   
--- index.js
+++ index.js
@@ -1,285 +1,166 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template.js');
-const { writeFile
-  // , copyFile 
-} = require('./utils/generate-site');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username:",
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address:",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter your email!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Enter your project title:",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("Please enter your project title!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project:",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("Please enter a description for your project!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Provide the steps required to install your project:",
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log("Please enter installation steps for your project!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions for your project.",
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log("Please provide usage inctructions for your project!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "confirm",
+        name: "confirmLicense",
+        message: "Would you like to include a license for your project?",
+        default: true
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Select a license for your project:",
+        choices: [
+            'Apache License 2.0',
+            'Boost Software License',
+            'CC-BY-4.0',
+            'CC-BY-SA-4.0',
+            'CCO-1.0',
+            'GNU GPLv2',
+            'GNU GPLv3',
+            'ISC',
+            'MIT',
+            'Mozilla Public License 2.0',
+            'SIL Open Font License 1.1',
+            'The Unlicense'
+        ],
+        when: ({ confirmLicense }) => {
+            if (confirmLicense) {
+            return true;
+            } else {
+            return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Do you have other Contributers you would like to credit for this project?",
+        validate: contributingInput => {
+            if (contributingInput) {
+                return true;
+            } else {
+                console.log("Please provide other Contributers you would like to credit for this project!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide examples on how to run tests you've written for your project:",
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log("Please provide testing examples!");
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (data) => {
+    fs.writeFile('./dist/README.md', data, err => {
+        if (err) {
+            console.log(err);
+        }
 
-// TODO: Create a function to initialize app
+        console.log('README generated!');
+    })
+}
+
 const init = () => {
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'What is the Title of your project? (Required)',
-        validate: titleInput => {
-          if (titleInput) {
-            return true;
-          } else {
-            console.log('Please enter the Title of your project!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a description of your project. (Required)',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('Please enter a description of your project!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'confirm',
-        name: 'confirmInstInstruct',
-        message: 'Do you have installation instructions?',
-        default: true
-      },
-      {
-        type: 'input',
-        name: 'instInstruct',
-        message: 'What are your installation instructions',
-        when: ({ confirmInstInstruct }) => confirmInstInstruct
-      },
-      {
-        type: 'input',
-        name: 'describeUsage',
-        message: 'Enter a description on how to use your project. (Required)',
-        validate: describeUsageInput => {
-          if (describeUsageInput) {
-            return true;
-          } else {
-            console.log('Please describe how to use your project!');
-            return false;
-          }
-        }
-      },
-      {
-          type: 'checkbox',
-          name: 'licensing',
-          message: 'What license would you like to classify your project as?',
-          choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-          validate: licensingInput => {
-            if (licensingInput) {
-              return true;
-            } else {
-              console.log('You have to select a license!');
-              return false;
-            }
-          }
-      }     
-    ]);
-  };
-  
-  const promptColab = portfolioData => {
-    if (!portfolioData.colaborators) {
-      portfolioData.colaborators = [];
-    }
-    return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'collabName',
-          message: 'Enter collaborators Name? (Required)',
-          validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('You need to enter your name!');
-              return false;
-            }
-          }
-        },
-        {
-          type: 'input',
-          name: 'githubUser',
-          message: 'Enter your GitHub username (Required)',
-          validate: githubUserInput => {
-            if (githubUserInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project description!');
-              return false;
-            }
-          }
-        },
-        {
-            type: 'input',
-            name: 'collabEmail',
-            message: 'Enter your email address (Required)',
-            validate: collabEmailInput => {
-              if (collabEmailInput) {
-                return true;
-              } else {
-                console.log('You need to enter an email!');
-                return false;
-              }
-            }
-          },
-        {
-          type: 'confirm',
-          name: 'confirmAddCollab',
-          message: 'Would you like to enter another collaborator?',
-          default: false
-        }
-      ])
-      .then(projectData => {
-        portfolioData.colaborators.push(projectData);
-        if (projectData.confirmAddCollab) {
-          return promptColab(portfolioData);
-        } else {
-          return portfolioData;
-        }
-      });
-  };
+    return inquirer.prompt(questions)
+    .then(readmeData => generateMarkdown(readmeData))
+    .then(markdownContent => writeToFile(markdownContent))
+    .catch(err => {
+        console.log(err);
+    });
+}
 
-  const promptScreenshot = portfolioData => {
-    if (!portfolioData.screenShot) {
-      portfolioData.screenShot = [];
-    }
-    return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'screenShotSRC',
-          message: 'Please proide the SRC for your screenshot to your project (Required)',
-          validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              return;
-            }
-          }
-        },
-        {
-          type: 'confirm',
-          name: 'confirmAddScreenShot',
-          message: 'Would you like to add another screenshot for your project?',
-          default: false
-        }
-
-      ])
-      .then(projectData => {
-        portfolioData.screenShot.push(projectData);
-        if (projectData.confirmAddScreenShot) {
-          return promptScreenshot(portfolioData);
-        } else {
-          return portfolioData;
-        }
-      });
-  };
-
-  const promptThirdParty = portfolioData => {
-    if (!portfolioData.thirdPartyInfo) {
-        portfolioData.thirdPartyInfo = [];
-      }
-      return inquirer
-        .prompt([
-            {
-                type: 'confirm',
-                name: 'confirmThirdParty',
-                message: 'Did you use any third-party assets that require attribution?',
-                default: false
-              },
-              {
-                type: 'input',
-                name: 'thirdParty',
-                message: 'What is the name of the third-party assets?',
-                when: ({ confirmThirdParty }) => confirmThirdParty
-              },
-
-              {
-                type: 'input',
-                name: 'thirdPartyLink',
-                message: 'What is the link to the third-party asset?',
-                when: ({ confirmThirdParty }) => confirmThirdParty
-              },
-              {
-                type: 'confirm',
-                name: 'addThirdParty',
-                message: 'Do you want to add another third-party assets that require attribution?',
-                default: false,
-                when: ({ confirmThirdParty }) => confirmThirdParty
-              },
-        ])
-
-        .then(projectData => {
-            portfolioData.thirdPartyInfo.push(projectData);
-            if (projectData.addThirdParty) {
-              return promptThirdParty(portfolioData);
-            } else {
-              return portfolioData;
-            }
-          });
- };
-
- const promptTutorials = portfolioData => {
-    if (!portfolioData.tutorialsInfo) {
-        portfolioData.tutorialsInfo = [];
-      }
-      return inquirer
-        .prompt([
-            {
-            type: 'confirm',
-            name: 'confirmTutorials',
-            message: 'Did you use any tutorials?',
-            default: false
-          },
-          {
-            type: 'input',
-            name: 'tutorials',
-            message: 'What is the name of the tutorial?',
-            when: ({ confirmTutorials }) => confirmTutorials
-          },
-          {
-            type: 'input',
-            name: 'tutorialsLink',
-            message: 'Enter the link of the tutorial?',
-            when: ({ confirmTutorials }) => confirmTutorials
-          },
-          {
-          type: 'confirm',
-          name: 'addTutorials',
-          message: 'Do you want to add another tutorial?',
-          default: false,
-          when: ({ confirmTutorials }) => confirmTutorials
-        },
-
-        ])
-
-        .then(projectData => {
-            portfolioData.tutorialsInfo.push(projectData);
-            if (projectData.addTutorials) {
-              return promptTutorials(portfolioData);
-            } else {
-              return portfolioData;
-            }
-          });
- };
-
-
-// Function call to initialize app
-init()
-  .then(promptColab)
-  .then(promptScreenshot)
-  .then(promptThirdParty)
-  .then(promptTutorials)
-
-  .then(portfolioData => {
-      // return portfolioData,
-    return generatePage(portfolioData);
-  })
-  .then(pageReadme => {
-    return writeFile(pageReadme);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+init();
