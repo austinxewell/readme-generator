ddfc6f94d4e9233ee1fe4dc288c5c1c3a1200f7e
Austin Ewell <austin.ewell86@gmail.com>
2024-09-28 12:32:52 -0600

    updated license that can be selected | short handed the license generation | created method that allows formatting list responses using | symbol

--- index.js
+++ index.js
@@ -1,166 +1,168 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        name: "username",
-        message: "Enter your GitHub username:",
-        validate: usernameInput => {
-            if (usernameInput) {
-                return true;
-            } else {
-                console.log("Please enter your GitHub username!");
-                return false;
-            }
-        }
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username:",
+    validate: (usernameInput) => {
+      if (usernameInput) {
+        return true;
+      } else {
+        console.log("Please enter your GitHub username!");
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter your email address:",
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log("Please enter your email!");
-                return false;
-            }
-        }
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter your email!");
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "title",
-        message: "Enter your project title:",
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log("Please enter your project title!");
-                return false;
-            }
-        }
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "Enter your project title:",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter your project title!");
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "description",
-        message: "Enter a description of your project:",
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log("Please enter a description for your project!");
-                return false;
-            }
-        }
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project:",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("Please enter a description for your project!");
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "installation",
-        message: "Provide the steps required to install your project:",
-        validate: installationInput => {
-            if (installationInput) {
-                return true;
-            } else {
-                console.log("Please enter installation steps for your project!");
-                return false;
-            }
-        }
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Provide the steps required to install your project. Separate your steps using a | symbol:",
+    validate: (installationInput) => {
+      if (installationInput) {
+        return true;
+      } else {
+        console.log("Please enter installation steps for your project!");
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "usage",
-        message: "Provide instructions for your project.",
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log("Please provide usage inctructions for your project!");
-                return false;
-            }
-        }
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "Provide instructions for your project. Separate your steps using a | symbol:",
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log("Please provide usage instructions for your project!");
+        return false;
+      }
     },
-    {
-        type: "confirm",
-        name: "confirmLicense",
-        message: "Would you like to include a license for your project?",
-        default: true
+  },
+  {
+    type: "confirm",
+    name: "confirmLicense",
+    message: "Would you like to include a license for your project?",
+    default: true,
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Select a license for your project:",
+    choices: [
+      "Creative Commons (Creative Work)",
+      "MIT (Permissive Open Source)",
+      "GPL (General Public License)",
+      "ISC (Permissive License)",
+      "Apache (Permissive License)",
+      "Proprietary (like Microsoft Windows)",
+      "None",
+    ],
+    when: ({ confirmLicense }) => {
+      if (confirmLicense) {
+        return true;
+      } else {
+        return false;
+      }
     },
-    {
-        type: "list",
-        name: "license",
-        message: "Select a license for your project:",
-        choices: [
-            'Apache License 2.0',
-            'Boost Software License',
-            'CC-BY-4.0',
-            'CC-BY-SA-4.0',
-            'CCO-1.0',
-            'GNU GPLv2',
-            'GNU GPLv3',
-            'ISC',
-            'MIT',
-            'Mozilla Public License 2.0',
-            'SIL Open Font License 1.1',
-            'The Unlicense'
-        ],
-        when: ({ confirmLicense }) => {
-            if (confirmLicense) {
-            return true;
-            } else {
-            return false;
-            }
-        }
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message:
+      "Do you have other Contributers you would like to credit for this project?",
+    validate: (contributingInput) => {
+      if (contributingInput) {
+        return true;
+      } else {
+        console.log(
+          "Please provide other Contributers you would like to credit for this project!"
+        );
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "contributing",
-        message: "Do you have other Contributers you would like to credit for this project?",
-        validate: contributingInput => {
-            if (contributingInput) {
-                return true;
-            } else {
-                console.log("Please provide other Contributers you would like to credit for this project!");
-                return false;
-            }
-        }
+  },
+  {
+    type: "input",
+    name: "tests",
+    message:
+      "Provide examples on how to run tests you've written for your project:",
+    validate: (testsInput) => {
+      if (testsInput) {
+        return true;
+      } else {
+        console.log("Please provide testing examples!");
+        return false;
+      }
     },
-    {
-        type: "input",
-        name: "tests",
-        message: "Provide examples on how to run tests you've written for your project:",
-        validate: testsInput => {
-            if (testsInput) {
-                return true;
-            } else {
-                console.log("Please provide testing examples!");
-                return false;
-            }
-        }
-    }
+  },
 ];
 
 // TODO: Create a function to write README file
 const writeToFile = (data) => {
-    fs.writeFile('./dist/README.md', data, err => {
-        if (err) {
-            console.log(err);
-        }
+  fs.writeFile("./dist/README.md", data, (err) => {
+    if (err) {
+      console.log(err);
+    }
 
-        console.log('README generated!');
-    })
-}
+    console.log("README generated!");
+  });
+};
 
 const init = () => {
-    return inquirer.prompt(questions)
-    .then(readmeData => generateMarkdown(readmeData))
-    .then(markdownContent => writeToFile(markdownContent))
-    .catch(err => {
-        console.log(err);
+  return inquirer
+    .prompt(questions)
+    .then((readmeData) => generateMarkdown(readmeData))
+    .then((markdownContent) => writeToFile(markdownContent))
+    .catch((err) => {
+      console.log(err);
     });
-}
+};
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,188 +1,113 @@
-const renderLicenseTitle = confirmLicense => {
+const renderLicenseTitleTOC = (license) => {
+  if (license === undefined || license === "None") {
+    return "";
+  }
+  return "- [License](#license)";
+};
+
+const renderLicenseTitle = (confirmLicense) => {
   if (confirmLicense) {
     return `# License`;
   } else {
-    return (``);
+    return ``;
   }
 };
 
-const renderLicenseTOC = confirmLicense => {
-  if (confirmLicense) {
-    return `* [License](#license)`;
-  } else {
-    return (``);
+const renderLicenseBadge = (license) => {
+  if (license === undefined || license === "None") {
+    return "";
   }
-}
+  const licenseStr = license.split(" (")[0].replace(/ /g, "_");
+  return `![Static Badge](https://img.shields.io/badge/License-${licenseStr}-blue)`;
+};
 
-const renderLicenseBadge = license => {
-  if (license === 'Apache License 2.0') {
-    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-  }
-  if (license === 'Boost Software License') {
-    return '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
-  }
-  if (license === 'CC-BY-4.0') {
-    return '[![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)';
-  }
-  if (license === 'CC-BY-SA-4.0') {
-    return '[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)';
-  }
-  if (license === 'CCO-1.0') {
-    return '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)';
-  }
-  if (license === 'GNU GPLv2') {
-    return '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
-  }
-  if (license === 'GNU GPLv3') {
-    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-  }
-  if (license === 'ISC') {
-    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
-  }
-  if (license === 'MIT') {
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-  }
-  if (license === 'Mozilla Public License 2.0') {
-    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
-  }
-  if (license === 'SIL Open Font License 1.1') {
-    return '[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)';
-  }
-  if (license === 'The Unlicense') {
-    return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
-  }
+const renderLicenseSection = (license) => {
   if (!license) {
-    return '';
+    return "";
   }
-}
-
-const renderLicenseLink = license => {
-  if (license === 'Apache License 2.0') {
-    return `View terms and conditions here: [Apache License 2.0](../utils/licenses/apachelicense.txt)`;
+  if (license.includes("Creative Commons")) {
+    return `### Creative Commons (CC) License
+    A Creative Commons (CC) software license allows creators to share their work with the public under specific terms, balancing the protection of their intellectual property with the freedom to share and use the work.`;
   }
-  if (license === 'Boost Software License') {
-    return `View terms and conditions here: [Boost Software License](../utils/licenses/boostlicense.txt)`;
-  }
-  if (license === 'CC-BY-4.0') {
-    return `View terms and conditions here: [CC-BY-4.0](../utils/licenses/CC-BY-4.0license.txt)`;
-  }
-  if (license === 'CC-BY-SA-4.0') {
-    return `View terms and conditions here: [CC-BY-SA-4.0](../utils/licenses/CC-BY-SA-4.0license.txt)`;
-  }
-  if (license === 'CCO-1.0') {
-    return `View terms and conditions here: [CCO-1.0](../utils/licenses/CCO-1.0license.txt)`;
-  }
-  if (license === 'GNU GPLv2') {
-    return `View terms and conditions here: [GNU GPLv2](../utils/licenses/GPLv2license.txt)`;
-  }
-  if (license === 'GNU GPLv3') {
-    return `View terms and conditions here: [GNU GPLv3](../utils/licenses/GPLv3license.txt)`;
-  }
-  if (license === 'ISC') {
-    return `View terms and conditions here: [ISC](../utils/licenses/ISClicense.txt)`;
-  }
-  if (license === 'MIT') {
-    return `View terms and conditions here: [MIT](../utils/licenses/MITlicense.txt)`;
-  }
-  if (license === 'Mozilla Public License 2.0') {
-    return `View terms and conditions here: [Mozilla Public License 2.0](../utils/licenses/mozillalicense.txt)`;
-  }
-  if (license === 'SIL Open Font License 1.1') {
-    return `View terms and conditions here: [SIL Open Font License 1.1](../utils/licenses/SILOFlicense.txt)`;
-  }
-  if (license === 'The Unlicense') {
-    return `View terms and conditions here: [The Unlicense](../utils/licenses/unlicense.txt)`;
-  }
-  if (!license) {
-    return '';
-  }
-}
-
-const renderLicenseSection = license => {
-  if (license === 'Apache License 2.0') {
-    return `## Apache License 2.0
-  A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`;
-  }
-  if (license === 'Boost Software License') {
-    return `## Boost Software License
-  A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`;
-  }
-  if (license === 'CC-BY-4.0') {
-    return `## Creative Commons Attribution 4.0 International
-  Permits almost any use subject to providing credit and license notice. Frequently used for media assets and educational materials. The most common license for Open Access scientific publications. Not recommended for software.`;
-  }
-  if (license === 'CC-BY-SA-4.0') {
-    return `## Creative Commons Attribution Share Alike 4.0 International
-  Similar to CC-BY-4.0 but requires derivatives be distributed under the same or a similar, compatible license. Frequently used for media assets and educational materials. A previous version is the default license for Wikipedia and other Wikimedia projects. Not recommended for software.`;
-  }
-  if (license === 'CCO-1.0') {
-    return `## Creative Commons Zero v1.0 Universal
-  The Creative Commons CC0 Public Domain Dedication waives copyright interest in a work you've created and dedicates it to the world-wide public domain. Use CC0 to opt out of copyright entirely and ensure your work has the widest reach. As with the Unlicense and typical software licenses, CC0 disclaims warranties. CC0 is very similar to the Unlicense.`;
-  }
-  if (license === 'GNU GPLv2') {
-    return `## GNU General Public License v2.0
-  The GNU GPL is the most widely used free software license and has a strong copyleft requirement. When distributing derived works, the source code of the work must be made available under the same license. There are multiple variants of the GNU GPL, each with different requirements.`;
-  }
-  if (license === 'GNU GPLv3') {
-    return `## GNU General Public License v3.0
-  Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.`;
-  }
-  if (license === 'ISC') {
-    return `## ISC License
-  A permissive license lets people do anything with your code with proper attribution and without warranty. The ISC license is functionally equivalent to the BSD 2-Clause and MIT licenses, removing some language that is no longer necessary.`;
-  }
-  if (license === 'MIT') {
-    return `## MIT License
+  if (license.includes("MIT")) {
+    return `### MIT License
   A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`;
   }
-  if (license === 'Mozilla Public License 2.0') {
-    return `## Mozilla Public License 2.0
-  Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.`;
+  if (license.includes("GPL")) {
+    return `### GNU General Public License
+  The GNU GPL is the most widely used free software license and has a strong copyleft requirement. When distributing derived works, the source code of the work must be made available under the same license. There are multiple variants of the GNU GPL, each with different requirements.`;
   }
-  if (license === 'SIL Open Font License 1.1') {
-    return `## SIL Open Font License 1.1
-  The Open Font License (OFL) is maintained by SIL International. It attempts to be a compromise between the values of the free software and typeface design communities. It is used for almost all open source font projects, including those by Adobe, Google and Mozilla.`;
+  if (license.includes("ISC")) {
+    return `### ISC License
+  A permissive license lets people do anything with your code with proper attribution and without warranty. The ISC license is functionally equivalent to the BSD 2-Clause and MIT licenses, removing some language that is no longer necessary.`;
   }
-  if (license === 'The Unlicense') {
-    return `## The Unlicense
+  if (license.includes("Apache")) {
+    return `### Apache License
+  A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`;
+  }
+  if (license.includes("Proprietary")) {
+    return `### Proprietary License
+    A proprietary software license grants the licensee (the user) limited rights to use, modify, or distribute the software while retaining exclusive ownership rights by the licensor (the creator or company).`;
+  }
+  if (license === "None") {
+    return `## No License
   A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.`;
   }
-  if (!license) {
-    return '';
+};
+
+const formatSteps = (answer) => {
+  if (!answer.includes("|")) {
+    return answer.trim();
   }
-}
 
-// TODO: Create a function to generate markdown for README
+  const steps = answer
+    .split("|")
+    .map((step) => step.trim())
+    .filter((step) => step !== "");
+
+  const formattedSteps = steps.map((step) => `- ${step}`);
+
+  return formattedSteps.join("\n  ");
+};
+
 function generateMarkdown(data) {
+  const {
+    username,
+    email,
+    title,
+    description,
+    installation,
+    usage,
+    confirmLicense,
+    license,
+    contributing,
+    tests,
+  } = data;
 
-  const { username, email, title, description, installation, usage, confirmLicense, license, contributing, tests } = data;
-
-  return `# ${title} 
-  ${renderLicenseBadge(license)}
-  # Description
+  return `# ${title} ${renderLicenseBadge(license)}
+  ## Description
   ${description}
-  # Table of Contents
-  * [Installation](#installation)
-  * [Usage](#usage)
-  ${renderLicenseTOC(confirmLicense)}
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  # Installation
-  ${installation}
-  # Usage
-  ${usage}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${renderLicenseTitleTOC()}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  ${formatSteps(installation)}
+  ## Usage
+  ${usage.trim()}
   ${renderLicenseTitle(confirmLicense)}
   ${renderLicenseSection(license)}
-  ${renderLicenseLink(license)}
-  # Contributing
+  ## Contributing
   ${contributing}
-  # Tests
+  ## Tests
   ${tests}
-  # Questions
-  * GitHub user: ${username}
+  ## Questions
+  If you have any questions regarding ${title}, please reach using the following methods:
   * https://github.com/${username}
-  * Reach me at ${email} for additional questions
+  * ${email}
   `;
 }
 
